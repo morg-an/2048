7661af5566e509f680cb6a33666f7bb8a241cdd7
Morgan Schumann <91375960+morg-an@users.noreply.github.com>
2022-12-21 17:24:58 -0600

    move shift() merge() and directional methods

--- Game.py
+++ Game.py
@@ -45,42 +45,6 @@ def draw(game_board):
             game_board.blit(text_surface, (tile.coordinate))
     pygame.display.update()
 
-def clear(fromTile):
-    #use to reset value and color of tile
-    fromTile.value = 0
-    fromTile.color = Constants.tile_colors[0]
-
-def shift(fromTile, toTile):
-    toTile.value = fromTile.value
-    toTile.color = fromTile.color
-    clear(fromTile)
-    print("Shifted from: Row ", fromTile.row, "Column ", fromTile.column)
-    print("shifted to: Row", toTile.row, "Column ", toTile.column)
-    #printGrid()
-    #set boolian to show that merge happened on keypress - used to verify before new rand tile generates.
-    return True
-
-def merge(fromTile, toTile):
-    #multiply value by 2 and increment color
-    toTile.value = fromTile.value*2
-    toTile.color = Constants.tile_colors[toTile.value]
-    clear(fromTile)
-    #mark that tile already changed to prevent the same tile from merging twice on same turn
-    toTile.changed = True
-    print("Merged from: Row ", fromTile.row, "Column ", fromTile.column)
-    print("Merged to: Row", toTile.row, "Column ", toTile.column)
-    #printGrid()
-    #set boolian to show that merge happened on keypress - used to verify before new rand tile generates.
-    return True
-
-def mergeOrShift(tile, comp_tile):
-    if comp_tile.value == tile.value and tile.changed == False:
-        return(merge(tile, comp_tile))
-    elif comp_tile.value == 0:
-        return(shift(tile, comp_tile))
-    else:
-        return False
-
 def isEndgame():
 # identify if all tiles contain value >= 2
     endgame = True
@@ -95,81 +59,6 @@ def reset():
         for tile in row:
             tile.changed = False
 
-def left():
-    validTurn = False
-    for row in tiles:
-        for tile in row:
-            if tile.column != 0 and tile.value != 0:
-                comp_tile = tiles[tile.row][tile.column-1]
-                if mergeOrShift(tile, comp_tile) == True:
-                    validTurn = True
-    removeLeft()
-    return validTurn
-
-def removeLeft():
-    for row in tiles:
-        for tile in row:
-            #check for zero value tiles that are not in the last column and where the tile to the right is non-zero
-            if tile.value == 0 and tile.column != Constants.tiles_across-1 and tiles[tile.row][tile.column+1].value != 0:
-                comp_tile = tiles[tile.row][tile.column+1]
-                shift(comp_tile, tile)
-
-def right():
-    validTurn = False
-    for row in tiles: 
-        for tile in reversed(row):
-            if tile.column != Constants.tiles_across-1 and tile.value !=0:
-                comp_tile = tiles[tile.row][tile.column+1]
-                if mergeOrShift(tile, comp_tile) == True:
-                    validTurn = True
-    removeRight()
-    return validTurn
-
-def removeRight():
-    for row in tiles:
-        for tile in row:
-            #check for zero value tiles that are not in the first column and where the tile to the left is non-zero
-            if tile.value == 0 and tile.column != 0 and tiles[tile.row][tile.column-1].value != 0:
-                comp_tile = tiles[tile.row][tile.column-1]
-                shift(comp_tile, tile)
-
-def up():
-    validTurn = False
-    for row in reversed(tiles):
-        for tile in row:
-            if tile.row != 0 and tile.value != 0:
-                comp_tile = tiles[tile.row-1][tile.column]
-                if mergeOrShift(tile, comp_tile) == True:
-                    validTurn = True
-    removeUp()
-    return validTurn
-
-def removeUp():
-    for row in tiles:
-        for tile in row:
-            #check for zero value tiles that are not in the last row and where the tile below is non-zero
-            if tile.value == 0 and tile.row != Constants.tiles_across-1 and tiles[tile.row+1][tile.column].value != 0:
-                comp_tile = tiles[tile.row+1][tile.column]
-                shift(comp_tile, tile)
-
-def down():
-    validTurn = False
-    for row in reversed(tiles):
-        for tile in row:
-            if tile.row != Constants.tiles_across-1 and tile.value != 0:
-                comp_tile = tiles[tile.row+1][tile.column]
-                if mergeOrShift(tile, comp_tile) == True:
-                    validTurn = True
-    removeDown()
-    return validTurn
-
-def removeDown():
-    for row in tiles:
-        for tile in row:
-            #check for zero value tiles that are not in the first row and where the tile above is non-zero
-            if tile.value == 0 and tile.row != 0 and tiles[tile.row-1][tile.column].value != 0:
-                comp_tile = tiles[tile.row-1][tile.column]
-                shift(comp_tile, tile)
 #event loop
 main()
 
@@ -182,19 +71,19 @@ while running:
             #listen for key presses
             if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 print("Right")
-                validTurn = right()
+                validTurn = TileManipulation.right(tiles)
                 print("Valid Turn?", validTurn)
             elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
                 print("Left")
-                validTurn = left()
+                validTurn = TileManipulation.left(tiles)
                 print("Valid Turn?", validTurn)
             elif event.key == pygame.K_UP or event.key == pygame.K_w:
                 print("Up")
-                validTurn = up()
+                validTurn = TileManipulation.up(tiles)
                 print("Valid Turn?", validTurn)
             elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                 print("Down")
-                validTurn = down()
+                validTurn = TileManipulation.down(tiles)
                 print("Valid Turn?", validTurn)
             elif event.key == pygame.K_ESCAPE:
                 print("You quit")
--- TileManipulation.py
+++ TileManipulation.py
@@ -14,3 +14,115 @@ def newRandTile(tiles):
     tiles[rand_x][rand_y].color = Constants.tile_colors[2]
     print("add value to random empty tile")
     return tiles[rand_x][rand_y]
+
+def clear(fromTile):
+    #use to reset value and color of tile
+    fromTile.value = 0
+    fromTile.color = Constants.tile_colors[0]
+
+def shift(fromTile, toTile):
+    toTile.value = fromTile.value
+    toTile.color = fromTile.color
+    clear(fromTile)
+    print("Shifted from: Row ", fromTile.row, "Column ", fromTile.column)
+    print("shifted to: Row", toTile.row, "Column ", toTile.column)
+    #printGrid()
+    #set boolian to show that merge happened on keypress - used to verify before new rand tile generates.
+    return True
+
+def merge(fromTile, toTile):
+    #multiply value by 2 and increment color
+    toTile.value = fromTile.value*2
+    toTile.color = Constants.tile_colors[toTile.value]
+    clear(fromTile)
+    #mark that tile already changed to prevent the same tile from merging twice on same turn
+    toTile.changed = True
+    print("Merged from: Row ", fromTile.row, "Column ", fromTile.column)
+    print("Merged to: Row", toTile.row, "Column ", toTile.column)
+    #printGrid()
+    #set boolian to show that merge happened on keypress - used to verify before new rand tile generates.
+    return True
+
+def mergeOrShift(tile, comp_tile):
+    if comp_tile.value == tile.value and tile.changed == False:
+        return(merge(tile, comp_tile))
+    elif comp_tile.value == 0:
+        return(shift(tile, comp_tile))
+    else:
+        return False
+
+def left(tiles):
+    validTurn = False
+    for row in tiles:
+        for tile in row:
+            if tile.column != 0 and tile.value != 0:
+                comp_tile = tiles[tile.row][tile.column-1]
+                if mergeOrShift(tile, comp_tile) == True:
+                    validTurn = True
+    removeLeft(tiles)
+    return validTurn
+
+def removeLeft(tiles):
+    for row in tiles:
+        for tile in row:
+            #check for zero value tiles that are not in the last column and where the tile to the right is non-zero
+            if tile.value == 0 and tile.column != Constants.tiles_across-1 and tiles[tile.row][tile.column+1].value != 0:
+                comp_tile = tiles[tile.row][tile.column+1]
+                shift(comp_tile, tile)
+
+def right(tiles):
+    validTurn = False
+    for row in tiles: 
+        for tile in reversed(row):
+            if tile.column != Constants.tiles_across-1 and tile.value !=0:
+                comp_tile = tiles[tile.row][tile.column+1]
+                if mergeOrShift(tile, comp_tile) == True:
+                    validTurn = True
+    removeRight(tiles)
+    return validTurn
+
+def removeRight(tiles):
+    for row in tiles:
+        for tile in row:
+            #check for zero value tiles that are not in the first column and where the tile to the left is non-zero
+            if tile.value == 0 and tile.column != 0 and tiles[tile.row][tile.column-1].value != 0:
+                comp_tile = tiles[tile.row][tile.column-1]
+                shift(comp_tile, tile)
+
+def up(tiles):
+    validTurn = False
+    for row in reversed(tiles):
+        for tile in row:
+            if tile.row != 0 and tile.value != 0:
+                comp_tile = tiles[tile.row-1][tile.column]
+                if mergeOrShift(tile, comp_tile) == True:
+                    validTurn = True
+    removeUp(tiles)
+    return validTurn
+
+def removeUp(tiles):
+    for row in tiles:
+        for tile in row:
+            #check for zero value tiles that are not in the last row and where the tile below is non-zero
+            if tile.value == 0 and tile.row != Constants.tiles_across-1 and tiles[tile.row+1][tile.column].value != 0:
+                comp_tile = tiles[tile.row+1][tile.column]
+                shift(comp_tile, tile)
+
+def down(tiles):
+    validTurn = False
+    for row in reversed(tiles):
+        for tile in row:
+            if tile.row != Constants.tiles_across-1 and tile.value != 0:
+                comp_tile = tiles[tile.row+1][tile.column]
+                if mergeOrShift(tile, comp_tile) == True:
+                    validTurn = True
+    removeDown(tiles)
+    return validTurn
+
+def removeDown(tiles):
+    for row in tiles:
+        for tile in row:
+            #check for zero value tiles that are not in the first row and where the tile above is non-zero
+            if tile.value == 0 and tile.row != 0 and tiles[tile.row-1][tile.column].value != 0:
+                comp_tile = tiles[tile.row-1][tile.column]
+                shift(comp_tile, tile)
